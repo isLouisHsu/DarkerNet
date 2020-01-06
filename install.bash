64e3dbde2b85fa2e2a8d6fead07491cba9e50ee8
louishsu <is.louishsu@foxmail.com>
2020-01-06 13:09:22 +0800

    install

---
+++ install.bash
@@ -0,0 +1,12 @@
+###
+ # @Description: 
+ # @Version: 1.0.0
+ # @Author: louishsu
+ # @E-mail: is.louishsu@foxmail.com
+ # @Date: 2020-01-06 13:08:28
+ # @LastEditTime : 2020-01-06 13:08:57
+ # @Update: 
+ ###
+make -j4
+sudo make uninstall
+sudo make install
